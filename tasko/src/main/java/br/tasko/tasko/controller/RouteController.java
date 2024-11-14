6449d8fc2dc681313b091d147c85b8d6afcd11f3
Salleeem <mateusonego@gmail.com>
2024-11-14 15:57:02 -0300

    A

--- tasko/src/main/java/br/tasko/tasko/controller/RouteController.java
+++ tasko/src/main/java/br/tasko/tasko/controller/RouteController.java
@@ -1,5 +1,26 @@
 package br.tasko.tasko.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RouteController {
     
+     // Rota para a página inicial (página de boas-vindas ou home)
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Retorna o nome da view (HTML) que será renderizada
+    }
+
+    // Rota para a página de cadastro
+    @GetMapping("/cadastro")
+    public String cadastro() {
+        return "cadastro"; // Retorna o nome da view de cadastro
+    }
+
+    // Rota para a página de login
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // Retorna o nome da view de login
+    }
 }
