bab28671f741320f7b8edbd2e913c482963416c4
Salleeem <mateusonego@gmail.com>
2024-11-14 14:19:40 -0300

    aaaa

--- tasko/src/main/java/br/tasko/tasko/model/User.java
+++ tasko/src/main/java/br/tasko/tasko/model/User.java
@@ -1,11 +1,14 @@
 package br.tasko.tasko.model;
 
 import java.io.Serializable;
+import java.sql.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity 
@@ -16,7 +19,23 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String tipo;
+    private Date data_nasc;
+    private String cep;
+    private String endereco;
+    private String foto;
 
+    @ManyToOne
+    @JoinColumn(name = "prestador_id", nullable = true)  // "nullable = true" torna a coluna opcional
+    private Prestador prestador;
+
+
+
+    //Métodos
     public Long getId() {
         return id;
     }
