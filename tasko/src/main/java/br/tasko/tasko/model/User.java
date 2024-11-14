59747e1db28c1cdaabe903ba5e523fd7cb110dfa
Salleeem <mateusonego@gmail.com>
2024-11-14 13:52:47 -0300

    first

---
+++ tasko/src/main/java/br/tasko/tasko/model/User.java
@@ -0,0 +1,35 @@
+package br.tasko.tasko.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name = "usuario") 
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ tasko/src/test/java/br/tasko/tasko/TaskoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tasko.tasko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
