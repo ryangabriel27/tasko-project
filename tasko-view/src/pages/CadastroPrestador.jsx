fd70c11aa247d1ba525661e96652ff4c3ab34f54
NicolasSouza21 <algumacoisan@gmail.com>
2024-11-21 15:46:00 -0300

    uc5

--- tasko-view/src/pages/CadastroPrestador.jsx
+++ tasko-view/src/pages/CadastroPrestador.jsx
@@ -1,89 +1,113 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import "../assets/css/CadastroPrestador.css";
+import taskoWhite from "../assets/img/TaskoWhite.png";
 
 const CadastroPrestador = () => {
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-        descricaoServicos: "",
-        categoriaServicos: "",
-        links: "",
-        valorHora: "",
-        cnpj: "",
-    });
-    const userId = sessionStorage.getItem("userId"); // Recupera o ID do sessionStorage
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    descricaoServicos: "",
+    categoriaServicos: "",
+    links: "",
+    valorHora: "",
+    cnpj: "",
+  });
+  const userId = sessionStorage.getItem("userId");
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({ ...prevData, [name]: value }));
-    };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!userId) {
-            alert("Erro: Usuário não encontrado.");
-            return;
-        }
-        try {
-            const response = await fetch("http://localhost:8080/api/prestadores", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ ...formData, usuario: { id: userId } }),
-            });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!userId) {
+      alert("Erro: Usuário não encontrado.");
+      return;
+    }
+    try {
+      const response = await fetch("http://localhost:8080/api/prestadores", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ...formData, usuario: { id: userId } }),
+      });
 
-            if (response.ok) {
-                alert("Cadastro de prestador realizado com sucesso!");
-                sessionStorage.removeItem("userId");
-                navigate("/auth");
-            } else {
-                const errorMessage = await response.text();
-                alert("Erro ao cadastrar prestador: " + errorMessage);
-            }
-        } catch (error) {
-            alert("Erro de conexão: " + error.message);
-        }
-    };
+      if (response.ok) {
+        alert("Cadastro de prestador realizado com sucesso!");
+        sessionStorage.removeItem("userId");
+        navigate("/auth");
+      } else {
+        const errorMessage = await response.text();
+        alert("Erro ao cadastrar prestador: " + errorMessage);
+      }
+    } catch (error) {
+      alert("Erro de conexão: " + error.message);
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h2>Cadastro de Prestador</h2>
+  return (
+    <div>
+      <nav className="navbar">
+        <Link to="/escolha-objetivo">
+          <button className="voltar-button">Voltar</button>
+        </Link>
+        <div className="logo-container">
+          <img src={taskoWhite} alt="tasko" className="logo" />
+        </div>
+      </nav>
+      <div className="content"> {/* Adicionei uma div com a classe content */}
+        <form onSubmit={handleSubmit} className="form">
+          <h2>Cadastro de Prestador</h2>
+          <div>
             <input
-                type="text"
-                name="descricaoServicos"
-                placeholder="Descrição dos Serviços"
-                required
-                onChange={handleInputChange}
+              type="text"
+              name="descricaoServicos"
+              placeholder="Descrição dos Serviços"
+              required
+              onChange={handleInputChange}
             />
+          </div>
+          <div>
             <input
-                type="text"
-                name="categoriaServicos"
-                placeholder="Categoria dos Serviços"
-                required
-                onChange={handleInputChange}
+              type="text"
+              name="categoriaServicos"
+              placeholder="Categoria dos Serviços"
+              required
+              onChange={handleInputChange}
             />
+          </div>
+          <div>
             <input
-                type="url"
-                name="links"
-                placeholder="Links de Portfólio"
-                required
-                onChange={handleInputChange}
+              type="url"
+              name="links"
+              placeholder="Links de Portfólio"
+              required
+              onChange={handleInputChange}
             />
+          </div>
+          <div>
             <input
-                type="number"
-                name="valorHora"
-                placeholder="Valor por Hora"
-                required
-                onChange={handleInputChange}
+              type="number"
+              name="valorHora"
+              placeholder="Valor por Hora"
+              required
+              onChange={handleInputChange}
             />
+          </div>
+          <div>
             <input
-                type="text"
-                name="cnpj"
-                placeholder="CNPJ"
-                required
-                onChange={handleInputChange}
+              type="text"
+              name="cnpj"
+              placeholder="CNPJ"
+              required
+              onChange={handleInputChange}
             />
-            <button type="submit">Cadastrar</button>
+          </div>
+          <button type="submit">Cadastrar</button>
         </form>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CadastroPrestador;
