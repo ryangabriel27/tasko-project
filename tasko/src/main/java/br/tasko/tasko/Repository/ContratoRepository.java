29615da4e1e9f6bc222f3bc012df69b51777e7f3
ryangabriel27 <ryanjulio56@gmail.com>
2024-12-02 14:07:37 -0300

    Excluido Serviço

--- tasko/src/main/java/br/tasko/tasko/Repository/ContratoRepository.java
+++ tasko/src/main/java/br/tasko/tasko/Repository/ContratoRepository.java
@@ -8,11 +8,16 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.tasko.tasko.model.Contrato;
+import br.tasko.tasko.model.Servico;
 import br.tasko.tasko.model.User;
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface ContratoRepository extends JpaRepository<Contrato, Long> {
     List<Contrato> findByUsuario(User usuario);
+
+    @Transactional
+    void deleteAllByServico(Servico servico);
 
     @Query("SELECT c FROM Contrato c WHERE c.servico.prestador.id = :prestadorId")
     List<Contrato> findByPrestadorId(@Param("prestadorId") Long prestadorId);
